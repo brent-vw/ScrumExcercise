104c1ac98f70da336ee1fef83a435d2047fde235
Brent Van Wynsberge <brent.vanwynsberge@realdolmen.com>
2018-09-28 16:25:33 +0200

    add bean access

--- src/main/java/com/realdolmen/scrum/RecipeBean.java
+++ src/main/java/com/realdolmen/scrum/RecipeBean.java
@@ -1,12 +1,17 @@
 package com.realdolmen.scrum;
 
+import com.realdolmen.scrum.domain.Ingredient;
 import com.realdolmen.scrum.domain.Recipe;
 import com.realdolmen.scrum.persistence.Repository;
+import com.realdolmen.scrum.utils.RecipeMatcher;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Named
 @RequestScoped
@@ -14,22 +19,37 @@ public class RecipeBean {
     @Inject
     Repository repository;
 
-    private Recipe recipe;
     private List<Recipe> allRecipes;
+    private List<Ingredient> allIngredients;
+    private List<Recipe> match;
+    private List<Ingredient> selected;
+    private RecipeMatcher matcher;
+
+    @PostConstruct
+    public void init() {
+        allRecipes = repository.getRecipes();
+        allIngredients = repository.getIngredients();
+
+        match = new ArrayList<>();
+        matcher = new RecipeMatcher();
+        selected = new ArrayList<>();
+    }
 
     public List<Recipe> getAllRecipes() {
         return allRecipes;
     }
 
-    public void setAllRecipes(List<Recipe> allRecipes) {
-        this.allRecipes = allRecipes;
+    public List<Ingredient> getAllIngredients() {
+        return allIngredients;
     }
 
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
+    public List<Recipe> getMatch() {
+        return match;
     }
 
-    public Recipe getRecipe(){
-        return null;
+    public void search() {
+        match = allRecipes.stream()
+                .filter(r -> matcher.canMake(selected, r))
+                .collect(Collectors.toList());
     }
 }
